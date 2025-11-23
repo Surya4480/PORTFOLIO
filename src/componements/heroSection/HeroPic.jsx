486f2a52d15842f0a043353c16c67348f4c7553b
Suryakantamondal <suryakantamondal73@gmail.com>
2025-11-23 20:44:27 +0530

    Refactor code structure for improved readability and maintainability

--- src/componements/heroSection/HeroPic.jsx
+++ src/componements/heroSection/HeroPic.jsx
@@ -12,7 +12,7 @@ const HeroPic = () => {
       className=" h-full flex items-center justify-center  "
     >
       <img
-        src="../../../public/images/cv-img.png"
+        src="../../../public/images/cv.png"
         alt="surya img"
         className="max-h-[455px] w-auto"
       />
